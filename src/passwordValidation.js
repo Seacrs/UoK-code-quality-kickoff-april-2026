c78c8cf8fbadde3bccff48fce3ec09178f60aa31
Seacrs <shemachris072@gmail.com>
2026-04-16 14:42:22 +0200

    feat: improve password validation logic and rules

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -10,17 +10,33 @@ export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
 
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
+  const validPassword = [...password];
+
+  if (validPassword.length !== 10) return false;
+  if(!/[a-zA-Z0-9]/.test(validPassword)) return false;
+  if(['@', '/', '#'].some(char => validPassword.includes(char))){
+    return false;
+  }
+  if(password == password.toLowerCase() || password == password.toUpperCase()){
+    return false;
+  }
+
+  for(let i = 1; i < validPassword.length - 1; i++ ){
+    const currNum = Number(validPassword[i])
+    const prevNum = Number(validPassword[i-1])
+    
+    if(isNaN(currNum) || isNaN(prevNum)){
+      continue;
+    }
+    
+    const res = Math.abs(currNum - prevNum)
+    if(res === 1){
+      return false;
+    }
+  }
+
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
+
   return true;
 }
