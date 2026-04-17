e8c52eb0a32c9fbf6fc2dbfda4201658cf6ff7a4
Seacrs <shemachris072@gmail.com>
2026-04-17 14:12:12 +0200

    refactor: improve password validation logic

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -9,34 +9,33 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
-
-  const validPassword = [...password];
-
-  if (validPassword.length !== 10) return false;
-  if(!/[a-zA-Z0-9]/.test(validPassword)) return false;
-  if(['@', '/', '#'].some(char => validPassword.includes(char))){
-    return false;
-  }
+  // The password is exactly 10 digits or characters;
+  if (password.length !== 10) return false;
+  // Must only contain Alphanumeric Characters
+  if(/[^a-zA-Z0-9]/.test(password)) return false;
+  // The password contain a mix of uppercase and lowercase characters;
   if(password == password.toLowerCase() || password == password.toUpperCase()){
     return false;
   }
-
-  for(let i = 1; i < validPassword.length - 1; i++ ){
-    const currNum = Number(validPassword[i])
-    const prevNum = Number(validPassword[i-1])
-    
-    if(isNaN(currNum) || isNaN(prevNum)){
-      continue;
+  // No descending or ascending sequences of length >= 3
+  let seq = [];
+  for(let i = 0; i <= password.length; i++){
+    const curr = password[i];
+    if(curr === undefined || isNaN(Number(curr))){
+      if(seq.length >= 3){
+        const isAsc = seq.every((n, i)=> i === 0 || n >  seq[i - 1]);
+        const isDesc = seq.every((n, i) => i === 0 || n < seq[i - 1]);
+        if(isDesc || isAsc ) return false;
+      }
+      seq = [];
     }
-    
-    const res = Math.abs(currNum - prevNum)
-    if(res === 1){
-      return false;
+    else{
+      seq.push(Number(curr));
     }
   }
-
+  // Must consist of at least 4 different digits/characters
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
-
+  
   return true;
 }
